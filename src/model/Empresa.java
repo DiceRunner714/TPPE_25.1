32ead2ff43eb13ad5e8ea245d9b6b7edd2e1b691
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-04-08 22:50:45 -0300

    add: cria classe empresa
    Co-authored-by: Víctor Moreira victormoreiraalmeida@gmail.com

---
+++ src/model/Empresa.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Empresa {
+    
+    private String nome;
+    private List<Biblioteca> filiais;
+
+    public Empresa(String nome){
+        this.nome = nome;
+        this.filiais = new ArrayList<>();
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public List<Biblioteca> getFiliais(){
+        return filiais;
+    }
+
+    public void adicionarBiblioteca(Biblioteca filial){
+        this.filiais.add(filial);
+    }
+
+    public void removerBiblioteca(int id){
+        Biblioteca biblioteca = null;
+
+        for(Biblioteca filial : filiais){
+            if(filial.getId() == id){
+                biblioteca = filial;
+            }
+        }
+
+        if(biblioteca != null){
+            this.filiais.remove(id);
+        } 
+    }
+
+    public Biblioteca buscarBiblioteca(int id){
+        for(Biblioteca filial : filiais){
+            if(filial.getId() == id){
+                return filial;
+            }
+        }
+        System.out.println("ERRO!!! Biblioteca NÃO encontrada.");
+        return null;
+    }
+
+    public void atualizarBiblioteca(int id, String nome, String local){
+        for(Biblioteca filial : filiais){
+            if(filial.getId() == id){
+                filial.setNome(nome);
+                filial.setLocal(local);
+            }
+        }
+    }
+
+    public void listarBibliotecas() {
+        if (filiais.isEmpty()) {
+            System.out.println("ERRO!!! Nenhuma biblioteca cadastrada.");
+        } else {
+            System.out.println("=== Bibliotecas da Empresa ===");
+            for (Biblioteca biblioteca : filiais) {
+                System.out.println(biblioteca);
+            }
+        }
+    }
+
+}
